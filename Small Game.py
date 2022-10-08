47e7e291639d0fcdc916382b3006e485b2ab3cfd
dipa03 <95011915+dipa03@users.noreply.github.com>
2022-10-07 20:50:21 -0500

    Update Small Game.py

--- Small Game.py
+++ Small Game.py
@@ -37,4 +37,4 @@ print('Thank you for playing, you got ', score, " questions correct. ")
 Mark = (score/total_q) * 100
 
 print("Mark: ", Mark)
-print ('Have a great day!"
+print ('Have a great day!")
